b68eac40ebf1c051a689a3e547e09aea5c19cced
nika1337 <nikanadaraia1337@gmail.com>
2024-05-20 12:54:27 +0400

    added data used for testing, it should be removed after roles and items are added

--- Web/Components/NavigationViewComponent.cs
+++ Web/Components/NavigationViewComponent.cs
@@ -1,13 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nika1337.Library.Infrastructure.Identity.Dtos;
 using Nika1337.Library.Infrastructure.Identity.Services;
-
 namespace Nika1337.Library.Web.Components;
 
 public class NavigationViewComponent(IdentityNavigationService navigationService) : ViewComponent
 {
+    private IEnumerable<NavigationMenuItemDto> testItems = [new NavigationMenuItemDto
+    {
+        Name = "Home",
+        Route = "/home",
+        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+    },
+    new NavigationMenuItemDto
+    {
+        Name = "About",
+        Route = "/about",
+        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+    },
+    new NavigationMenuItemDto
+    {
+        Name = "Services",
+        Route = "/services",
+        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>
+        {
+            new NavigationMenuItemDto
+            {
+                Name = "Consulting",
+                Route = "/services/consulting",
+                ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+            },
+            new NavigationMenuItemDto
+            {
+                Name = "Support",
+                Route = "/services/support",
+                ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+                {
+                    new NavigationMenuItemDto
+                    {
+                        Name = "Home",
+                        Route = "/home",
+                        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+                    },
+                    new NavigationMenuItemDto
+                    {
+                        Name = "About",
+                        Route = "/about",
+                        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+                    },
+                }
+            }
+        }
+    },
+    new NavigationMenuItemDto
+    {
+        Name = "Contact",
+        Route = "/contact",
+        ChildNavigationMenuItemDtos = new List<NavigationMenuItemDto>()
+    }];
+
    public async Task<IViewComponentResult> InvokeAsync()
    {
-        var navigationMenuItems = await navigationService.GetNavigationMenuItems(UserClaimsPrincipal);
+        //var navigationMenuItems = await navigationService.GetNavigationMenuDtos(UserClaimsPrincipal);
+        var navigationMenuItems = testItems;
         return View(navigationMenuItems);
    }
 }
