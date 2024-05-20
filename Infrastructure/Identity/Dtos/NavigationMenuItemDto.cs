1315b45d7dda63e656feeea97d50b680b5e82a6f
nika1337 <nikanadaraia1337@gmail.com>
2024-05-20 12:46:31 +0400

    added Algorithm of mapping from NavigationMenuItem to its dto

---
+++ Infrastructure/Identity/Dtos/NavigationMenuItemDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Nika1337.Library.Infrastructure.Identity.Dtos;
+
+public class NavigationMenuItemDto
+{
+    public required string Name { get; set; }
+    public required string Route { get; set; }
+    public ICollection<NavigationMenuItemDto> ChildNavigationMenuItemDtos { get; set; } = [];
+}
--- Infrastructure/Identity/Services/IdentityNavigationService.cs
+++ Infrastructure/Identity/Services/IdentityNavigationService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Nika1337.Library.Infrastructure.Identity.Dtos;
 using Nika1337.Library.Infrastructure.Identity.Entities;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +14,7 @@ public class IdentityNavigationService(UserManager<Employee> userManager, RoleMa
     private readonly UserManager<Employee> _userManager = userManager;
     private readonly RoleManager<EmployeeRole> _roleManager = roleManager;
 
-    public async Task<IEnumerable<NavigationMenuItem>> GetNavigationMenuItems(ClaimsPrincipal claimsPrincipal)
+    public async Task<IEnumerable<NavigationMenuItemDto>> GetNavigationMenuDtos(ClaimsPrincipal claimsPrincipal)
     {
         if (claimsPrincipal == null)
         {
@@ -25,9 +27,26 @@ public class IdentityNavigationService(UserManager<Employee> userManager, RoleMa
         {
             return [];
         }
-        // Get role names assigned to the employee
+
+        // Ensure distinct menu items
+        return await GetNavigationMenuDtos(employee);
+    }
+
+    private async Task<IList<NavigationMenuItemDto>> GetNavigationMenuDtos(Employee employee)
+    {
         var roleNames = await _userManager.GetRolesAsync(employee);
 
+        var menuItems = await GetNavigationMenuItems(roleNames);
+
+        var parentItems = FilterNonAccessibleChildren(menuItems);
+
+        var menuItemDtos = MapToDtos(parentItems);
+
+        return menuItemDtos;
+    }
+
+    private async Task<IList<NavigationMenuItem>> GetNavigationMenuItems(IList<string> roleNames)
+    {
         // Query roles and navigation menu items
         var menuItems = new List<NavigationMenuItem>();
 
@@ -41,8 +60,76 @@ public class IdentityNavigationService(UserManager<Employee> userManager, RoleMa
                 menuItems.AddRange(role.NavigationMenuItems);
             }
         }
+        return menuItems;
+    }
 
-        // Ensure distinct menu items
-        return menuItems.Distinct().ToList();
+
+
+
+    private IList<NavigationMenuItem> FilterNonAccessibleChildren(IList<NavigationMenuItem> menuItems)
+    {
+        var parentItems = menuItems.Where(nmi => nmi.ParentNavigationMenuItem is null).ToList();
+
+        var childItems = menuItems.Where(nmi => nmi.ParentNavigationMenuItem is not null).ToList();
+
+        foreach (var parentItem in parentItems)
+        {
+            RemoveAllChildrenNotIn(parentItem, childItems);
+        }
+        return parentItems;
+    }
+
+    private void RemoveAllChildrenNotIn(NavigationMenuItem parentItem, IList<NavigationMenuItem> childItems)
+    {
+        if (childItems.IsNullOrEmpty())
+        {
+            parentItem.ChildNavigationMenuItems?.Clear();
+            return;
+        }
+        else if (parentItem.ChildNavigationMenuItems.IsNullOrEmpty()) {
+            return;
+        }
+        else
+        {
+            foreach (var childItem in parentItem.ChildNavigationMenuItems)
+            {
+                if (childItems.Remove(childItem))
+                {
+                    RemoveAllChildrenNotIn(childItem, childItems);
+                }
+                else
+                {
+                    parentItem.ChildNavigationMenuItems.Remove(childItem);
+                }
+            }
+        }
+    }
+    private IList<NavigationMenuItemDto> MapToDtos(IList<NavigationMenuItem> menuItems)
+    {
+        var dtos = new List<NavigationMenuItemDto>();
+        foreach (var menuItem in menuItems) {
+            var dto = MapToDto(menuItem);
+            dtos.Add(dto);
+        }
+        return dtos;
+    }
+
+    private NavigationMenuItemDto MapToDto(NavigationMenuItem menuItem)
+    {
+        var dto = new NavigationMenuItemDto
+        {
+            Name = menuItem.Name,
+            Route = menuItem.Route
+        };
+
+        foreach (var childItem in menuItem.ChildNavigationMenuItems)
+        {
+            var childDto = MapToDto(childItem); // Recursive call
+            dto.ChildNavigationMenuItemDtos.Add(childDto);
+        }
+
+        return dto;
     }
 }
+
+// [(null, 1), (null,2), (null,3), (1,4), (1,5), (2,6), (6,7)]
